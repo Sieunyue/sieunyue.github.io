d1f2f9789d5b48a7d25c75639ecffeec395a444f
Sieunyue <sieunyue@gmail.com>
2023-07-14 10:38:50 +0800

    update

--- next.config.js
+++ next.config.js
@@ -3,12 +3,10 @@ const PostMetaPlugin = require('./lib/meta')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/page',
   images:{
     unoptimized: true
   },
   webpack(config, options) {
-    
     config.plugins.push(new PostMetaPlugin())
  
     return config
